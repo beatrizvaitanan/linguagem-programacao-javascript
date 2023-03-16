bf3db92961b6fa0f465f586f9428ea0816845614
beatrizvaitanan <128067877+beatrizvaitanan@users.noreply.github.com>
2023-03-16 12:52:12 -0300

    Revert "Alterando mensagem do fim do loop"
    This reverts commit 8a2bcec9e9b8a8d5ef0f8cf81e6f87224d839f77.

--- controle/while_4.js
+++ controle/while_4.js
@@ -5,6 +5,6 @@ while(controle <= 10) {
     console.log(controle);
     controle++;
 }
-console.log("fim do loop");
+console.log("fim");
 
 //declaração; expressão; incremento -> quantidade determinada 
